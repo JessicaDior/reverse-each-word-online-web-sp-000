7b70a02679fe9b77f2c8d3bc361376b00f707362
Jc Dior <jessicaclairdior@gmail.com>
2020-08-27 22:22:26 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,9 @@
 def reverse_each_word(string)
+  new_array = []
   array = string.split
   array.each do |word|
     word.reverse
+    new_array<<array
   end
-  return array
+  return new_array
 end
